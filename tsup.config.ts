198455626a3f3250f551bb14fbbd66418f75537a
Anik <anikdey2024@gmail.com>
2026-04-27 16:06:29 +0600

    config updated

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ['index.ts'],
+  entry: ['src/index.ts'],
   external: ['react', 'react-dom'],
   format: ['esm', 'cjs'],
   outDir: 'dist',
